d1bb079339b81829db09543267bfdcb0b4e3eb95
sazzad-anwar <sazzadzihan@gmail.com>
2020-09-09 14:52:27 +0600

    ip check

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send(req.ip))
+app.get('/', (req, res) => res.send(`Your IP address is ${req.ip}`))
 app.listen(port, () => console.log(`Example app listening on port port!`))
