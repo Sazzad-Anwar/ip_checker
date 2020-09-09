73a4190dcba4b94eea565b964dc417ef00e91134
sazzad-anwar <sazzadzihan@gmail.com>
2020-09-09 15:06:42 +0600

    ip checker updated

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+var ip = require('ip');
+ 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send(`Your IP address is ${req.ip}`))
+app.get('/', (req, res) => res.send(`Your IP address is ${ip.address()}`))
 app.listen(port, () => console.log(`Example app listening on port port!`))
